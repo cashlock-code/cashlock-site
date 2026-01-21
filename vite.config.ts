70fbd00c01ce60255f7dc67de66544cbcd13092d
Christian Ashlock <cashlock@gmail.com>
2026-01-21 15:17:36 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // GitHub Actions will set the correct base automatically for Pages,
 // but this fallback helps local builds if needed.
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+// const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
 
 export default defineConfig({
   plugins: [react()],
+  //  base: repoName ? `/${repoName}/` : "/",
   base: "/",
 });
